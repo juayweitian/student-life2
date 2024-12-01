a93da42cf58654b9db33e3f42d265de73753bee9
juayweitian <juayweitian5@gmail.com>
2024-12-01 13:42:39 +0800

    Completed the setup of testing environment

---
+++ index-test.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var app = express();
+
+const PORT = process.env.PORT || 5050
+var startPage = "index.html";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("./instrumented"));
+
+
+const { addCourse, viewCourses, editCourses } = require("./utils/CoursesUtil")
+app.post("/add-course", addCourse);
+app.get("/view-courses", viewCourses);
+app.put('/edit-course/:id', editCourses);
+
+const { addStudent, viewStudents, editStudent} = require('./utils/StudentUtils')
+app.post('/add-students', addStudent);
+app.get('/view-students', viewStudents);
+app.put('/edit-student/:id', editStudent);
+
+
+app.get('/', (req, res) => {
+res.sendFile(__dirname + "/instrumented/" + startPage);
+})
+
+server = app.listen(PORT, function () {
+const address = server.address();
+const baseUrl = `http://${address.address == "::" ? 'localhost' :
+address.address}:${address.port}`;
+console.log(`Demo project at: ${baseUrl}`);
+});
+
+module.exports = {app, server}
